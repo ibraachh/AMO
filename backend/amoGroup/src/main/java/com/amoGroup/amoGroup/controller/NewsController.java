376482c38042c648f9466720c8d4a8126fc9006a
aaphar <apharmagaramova@gmail.com>
2024-09-24 15:19:24 +0400

    news controller

---
+++ backend/amoGroup/src/main/java/com/amoGroup/amoGroup/controller/NewsController.java
@@ -0,0 +1,160 @@
+package com.amoGroup.amoGroup.controller;
+
+import com.amoGroup.amoGroup.entities.News;
+import com.amoGroup.amoGroup.patch.Patcher;
+import com.amoGroup.amoGroup.repositories.NewsRepository;
+import com.amoGroup.amoGroup.response.MessageResponse;
+import com.amoGroup.amoGroup.response.NewsResponse;
+import com.amoGroup.amoGroup.services.news.NewsService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+
+    @Autowired
+    NewsService newsService;
+
+    @Autowired
+    NewsRepository newsRepository;
+
+    @Autowired
+    Patcher patcher;
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @PostMapping("/create")
+    public ResponseEntity<News> create(@Valid @RequestBody News request) {
+        try {
+            News news = newsService.add(request);
+            return ResponseEntity.ok(news);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @PostMapping("/update")
+    public ResponseEntity<News> update(@Valid @RequestBody News request) {
+        try {
+            News news = newsService.update(request);
+            return ResponseEntity.ok(news);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
+    public ResponseEntity<News> patch(@PathVariable String id, @RequestBody News patch) {
+        try {
+            News news = newsRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("News with this id does not exist"));
+            patcher.patcher(news, patch);
+            return ResponseEntity.ok(newsRepository.save(news));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        try {
+            boolean result = newsService.delete(id);
+            if (result) {
+                return ResponseEntity.ok(new MessageResponse(HttpStatus.OK, "News entity is deleted successfully"));
+            }
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<NewsResponse> get(@RequestHeader(value = "Accept-Language", defaultValue = "az") String language, @PathVariable String id) {
+        try {
+            NewsResponse u = newsService.get(id, language)
+                    .orElseThrow(() -> new RuntimeException("News with this id does not exist"));
+            return ResponseEntity.ok(u);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<NewsResponse>> list(@RequestHeader(value = "Accept-Language", defaultValue = "az") String language) {
+        try {
+            return ResponseEntity.ok(newsService.list(language));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @GetMapping("/listAll")
+    public ResponseEntity<List<News>> listAllByType() {
+        try {
+            return ResponseEntity.ok(newsService.listAll());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @GetMapping("/getWithTranslation/{id}")
+    public ResponseEntity<News> get(@PathVariable String id) {
+        try {
+            News u = newsService.getNewsWithTranslations(id)
+                    .orElseThrow(() -> new RuntimeException("Entity not found with this id"));
+            return ResponseEntity.ok(u);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "authentication")
+    @GetMapping("/count")
+    public ResponseEntity<?> getCount() {
+        try {
+            return ResponseEntity.ok(newsService.count());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MessageResponse(HttpStatus.BAD_REQUEST, e.getMessage()));
+        }
+    }
+}
--- backend/amoGroup/src/main/java/com/amoGroup/amoGroup/services/news/NewsService.java
+++ backend/amoGroup/src/main/java/com/amoGroup/amoGroup/services/news/NewsService.java
@@ -1,4 +1,25 @@
 package com.amoGroup.amoGroup.services.news;
 
+import com.amoGroup.amoGroup.entities.News;
+import com.amoGroup.amoGroup.response.NewsResponse;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface NewsService {
+    News add(News news);
+
+    News update(News news);
+
+    boolean delete(String id);
+
+    Optional<NewsResponse> get(String id, String language);
+
+    Optional<News> getNewsWithTranslations(String id);
+
+    List<NewsResponse> list(String language);
+
+    List<News> listAll();
+
+    long count();
 }
