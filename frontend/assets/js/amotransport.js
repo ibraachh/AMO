fc8c2bbbfe41808f446c590c9d7cb9de031c4535
suleymanovemin <suleymanov.emin2001@gmail.com>
2024-10-08 15:36:37 +0400

    social medias

--- frontend/assets/js/amotransport.js
+++ frontend/assets/js/amotransport.js
@@ -23,3 +23,32 @@ getList().then((data) => {
     card.textContent = item.title;
   });
 });
+
+const socialIcons = document.querySelector(".sosial-links");
+async function fetchSocialMedia() {
+  try {
+    const headers = {
+      "x-api-key": "aebddf40-4255-4a9a-8bdb-3eea84c28bb9",
+    };
+    const response = await fetch(
+      "https://api.studentall.az:9851/api/contact-info/list",
+      { headers }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("response", data);
+
+    Object.entries(data[0].socials).forEach(([key, value]) => {
+      const socialIcon = document.createElement("li");
+      socialIcon.innerHTML = `<a href="${value}" target="_blank"><i class="fa-brands fa-${key.toLocaleLowerCase()} text-[20px] hover:text-red-300 duration-200 "></i></a>`;
+      socialIcons.append(socialIcon);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchSocialMedia();
